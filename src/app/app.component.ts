4479cd388c75acadf85ba213290f23addc48be59
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-02-28 22:38:09 -0300

    testando uma calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent{
+  nome: string = "Eduardo A. Cruz Junior";
+  result: number = 0;
+
+  calcular(valor1: number, valor2: number) : number {
+     return valor1 + valor2;
+  }
+
+  clique() {
+    //usar o getElementById ou Name do javascript para resolver isso
+    //this.result = calcular();
+  }
 
 }
